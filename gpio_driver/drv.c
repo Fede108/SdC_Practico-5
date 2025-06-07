bd0edfc4f434e9949481013e72a07191b3b5bbf4
Fede108 <federico.cechich@mi.unc.edu.ar>
2025-06-07 14:27:26 -0300

    buffer de toda la señal

--- gpio_driver/drv.c
+++ gpio_driver/drv.c
@@ -11,6 +11,7 @@
 
 #define DEVICE_NAME "rpi_gpio"
 #define CLASS_NAME "my_gpio"
+#define BUFFER_SIZE 64
 
 static dev_t devnum; 		// Global variable for the first device number
 static struct cdev c_dev; 	// Global variable for the character device structure
@@ -25,12 +26,10 @@ static struct class *cl; 	// Global variable for the device class
 /* gpios variables*/
 struct gpio_desc *in0, *in1;
 
-/*active pin*/
-#define PIN_0     0
-#define PIN_1     1
 
-static int active_pin = PIN_0;
-
+/* Buffer para muestrear la señal */
+static unsigned char sample_buf[BUFFER_SIZE];
+static size_t buf_len = BUFFER_SIZE;
 
 
 /* declarations probe and remove fuctions */
@@ -93,38 +92,15 @@ static int my_close(struct inode *i, struct file *f)
 
 static int my_open(struct inode *i, struct file *f)
 {
-    printk(KERN_INFO "Driver3_SdeC: open()\n");
+    printk(KERN_INFO DEVICE_NAME": open()\n");
     return 0;
-}
-
-static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
-{
-    int value;
-    unsigned char out;
-
-    /* solo un byte de salida */
-    if (*off > 0 || len < 1)
-        return 0;
-
-    /* leer el GPIO correspondiente */
-    if (active_pin == PIN_0)
-        value = gpiod_get_value(in0);
-    else
-        value = gpiod_get_value(in1);
-
-    /* Convertir a byte */
-    out = (value != 0) ? 1 : 0;
-
-    if (copy_to_user(buf, &out, 1))
-        return -EFAULT;
-
-    (*off)++;
-    return 1;
 }
 
 static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
     unsigned char user_val;
+    struct gpio_desc *active_gpio;
+    size_t i;
 
     /* esperamos 1 byte con 0 o 1 */
     if (len < 1)
@@ -134,18 +110,39 @@ static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff
         return -EFAULT;
 
     switch (user_val) {
-        case 0:
-            active_pin = PIN_0;
+        case '0':
+            active_gpio = in0;
             break;
-        case 1:
-            active_pin = PIN_1;
+        case '1':
+            active_gpio = in1;
             break;
         default:
             /* valor no válido, lo ignoramos o devolvemos error */
             return -EINVAL;
     }
 
+    /* Muestreamos BUFFER_SIZE veces rápidamente */
+    for (i = 0; i < BUFFER_SIZE; i++) {
+        sample_buf[i] = gpiod_get_value(active_gpio) ? '1' : '0';
+        //udelay(10);
+    }
+
     return len;
+}
+
+static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+    ssize_t to_copy = min(buf_len - *off, len);
+
+    if (to_copy <= 0)
+        return 0;
+
+    /* read data from kernel buffer to user buffer */
+    if (copy_to_user(buf, sample_buf + *off, to_copy))
+        return -EFAULT;
+
+    *off += to_copy;
+    return to_copy;
 }
 
 
@@ -158,45 +155,44 @@ static struct file_operations pugs_fops =
     .write = my_write
 };
 
-static int __init drv_init(void) /* Constructor */
+static int __init drv_init(void)
 {
-    int return_value;
+    int ret;
     struct device *dev_ret;
 
-    if(platform_driver_register(&my_driver)) 
-    {
-		printk("Rpi_gpio_driver - Error! Could not load driver\n");
-		return -1;
-	}
+    if ((ret = alloc_chrdev_region(&devnum, 0, 1, DEVICE_NAME)) < 0)
+        return ret;
 
-    if ((return_value = alloc_chrdev_region(&devnum, 0, 1, DEVICE_NAME)) < 0)
-    {
-        return return_value;
-    }
-
-    if (IS_ERR(cl = class_create(THIS_MODULE, CLASS_NAME)))
-    {
+    cl = class_create(CLASS_NAME);
+    if (IS_ERR(cl)) {
         unregister_chrdev_region(devnum, 1);
         return PTR_ERR(cl);
     }
 
-    if (IS_ERR(dev_ret = device_create(cl, NULL, devnum, NULL, DEVICE_NAME)))
-    {
+    dev_ret = device_create(cl, NULL, devnum, NULL, DEVICE_NAME);
+    if (IS_ERR(dev_ret)) {
         class_destroy(cl);
         unregister_chrdev_region(devnum, 1);
         return PTR_ERR(dev_ret);
     }
 
     cdev_init(&c_dev, &pugs_fops);
-    if ((return_value = cdev_add(&c_dev, devnum, 1)) < 0)
-    {
+    if ((ret = cdev_add(&c_dev, devnum, 1)) < 0) {
         device_destroy(cl, devnum);
         class_destroy(cl);
         unregister_chrdev_region(devnum, 1);
-        return return_value;
+        return ret;
     }
 
-    printk(KERN_INFO "Rpi_gpio_driver: Registrado exitosamente..!!\n");
+    if ((ret = platform_driver_register(&my_driver)) < 0) {
+        cdev_del(&c_dev);
+        device_destroy(cl, devnum);
+        class_destroy(cl);
+        unregister_chrdev_region(devnum, 1);
+        return ret;
+    }
+
+    printk(KERN_INFO DEVICE_NAME ": Registrado exitosamente\n");
     return 0;
 }
 
